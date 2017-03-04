69d3b11e18e157361c54a022ce69ef99e34c1e11
Ian de Jesus <ian.dejesus.md@gmail.com>
2017-03-04 10:29:21 +0800

    Added data types and control structure examples

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Integer value = 1;
+        Double valueDouble = 23.1414;
+        Integer value2 = 2;
+        Double sum = Double.valueOf(value2 + value);
+        Boolean isCar = true;
+
+
+        if(isCar){
+            System.out.println("car is true");
+        }
+
+        System.out.println(sum);
+
+        System.out.println(value);
+        System.out.println(valueDouble);
         System.out.println("Hello World");
     }
 }
